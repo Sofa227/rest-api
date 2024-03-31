a1a6fb6c0d367a9c0d1a105a1a3b211746902bf9
GoreOtUma <142909774+GoreOtUma@users.noreply.github.com>
2024-03-31 15:28:52 +0300

    Add files via upload

--- Marina/index.php
+++ Marina/index.php
@@ -8,14 +8,16 @@
 
 </head>
 <body>
+<div class="cd-user-modal">
+	<div class="cd-user-modal-container">
     <div id="cd-login">
         <form class="cd-form"  action="signin.php" method="POST">
             <p class="fieldset">
-                <input class="full-width has-padding has-border" id="signin-email" type="text" placeholder="Логин" name="signin_login">
+                <input class="full-width has-padding has-border" id="signin_login" type="text" placeholder="Логин" name="signin_login">
             </p>
     
             <p class="fieldset">
-                <input class="full-width has-padding has-border" id="signin-password" type="text" placeholder="Пароль" name="signin_password">
+                <input class="full-width has-padding has-border" id="signin_password" type="text" placeholder="Пароль" name="signin_password">
                 <a href="#0" class="hide-password">Скрыть</a>
             </p>
     
@@ -23,6 +25,8 @@
                 <input class="full-width" type="submit" value="Войти">
             </p>
         </form>
+</div>
+</div>
     </div> 
     <script src="main.js"></script>
 </body>
