dfa0f60790c73e36b8607f29cf9d7f62535f40ab
divan <ivdr04@mai.ru>
2024-03-31 16:50:22 +0300

    asdasd

--- Marina/main_window.php
+++ Marina/main_window.php
@@ -124,13 +124,15 @@
         </form>
         
         <h2>Управление платежами и расходами</h2>
-        <form id="managePaymentsForm">
+
+        <form id="managePaymentsForm" action="deletepayment.php">
             <input type="text" id="clientIdPayment" name="clientIdPayment" placeholder="ID клиента">
             <p>
-                <button type="submit">Удалить последний платеж</button>
+                <button type="submit" name ="deletePayment">Удалить последний платеж</button>
             </p>
             <p>
-                <button type="submit">Удалить последний расход</button>
+                <button type="submit" name = "deleteExpense">Удалить последний расход</button>
+
             </p>
             <p>
                 <select name="clientPaymentNew" id="clientPaymentNew" >
@@ -145,10 +147,11 @@
                     <option value="clientPaymentNew8">СБП</option>
                 </select>
                 <input type="number" id="addIngo" name="addIngo" placeholder="Сумма платежа">
-                <button type="submit">Добавить платеж</button>
+
+                <button type="submit" name = "addPayment">Добавить платеж</button>
             </p>
             <p>
-                <select name="clientServiceNew" id="clientPaymentNew" >
+                <select name="clientServiceNew" id="clientServiceNew" >
                     <option value="clientServiceNew0">-- Услуги с периодом списания денежных средств --</option>
                     <option value="clientServiceNew1">Услуга – Видеонаблюдение</option>
                     <option value="clientServiceNew2">Услуга – Домофония</option>ъъ
@@ -160,7 +163,9 @@
                     <option value="clientServiceNew8">Услуга – Хостинг веб-ресурсов</option>
                 </select>
                 <input type="number" id="addOutgo" name="addOutgo" placeholder="Сумма расхода">
-                <button type="submit">Добавить расход</button>
+
+                <button type="submit" name ="addExpense">Добавить расход</button>
+
             </p>
         </form>
         
@@ -178,16 +183,20 @@
         
         <h2>Получение информации о текущем статусе клиента</h2>
         <form id="getClientStatusForm" action="receive_inf.php" onsubmit="return getClientInfo()">
+
             <input type="email" id="email" name="email" placeholder="Email клиента">
+
             <button type="submit">Получить статус клиента</button>
             <p><textarea id="infoClientStatus" name="newInfo" readonly></textarea></p>
         </form>
+
 
 
         <script>
         function getClientInfo() {
             // Получаем email клиента из поля ввода
             var email = document.getElementById("email").value;
+
 
             // Создаем объект XMLHttpRequest для выполнения запроса к серверу
             var xhr = new XMLHttpRequest();
@@ -203,8 +212,10 @@
                 }
             };
 
+
             // Формируем URL для запроса к серверу, добавляем параметр email
             var url = 'get_inf.php?email=' + encodeURIComponent(email);
+
 
             // Открываем соединение и отправляем запрос к серверу
             xhr.open('GET', url, true);
@@ -258,6 +269,7 @@
         
         <h2>Автоматическое списание денежных средств</h2>
         <form id="autoDebitForm" action="debit.php" method="post">
+
             <input type="text" id="autoDebitClientId" name="autoDebitClientId" placeholder="ID клиента">
             <button type="submit">Списать деньги по расписанию</button>
         </form> 
